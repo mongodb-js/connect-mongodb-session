4ee84ad74f73358958f91d31afc542702d25aafe
Valeri Karpov <valeri.karpov@mongodb.com>
2015-10-18 10:52:17 -0400

    Fix #19

--- setup-tests.sh
+++ setup-tests.sh
@@ -1,6 +1,6 @@
 mkdir -p vendor
 cd vendor
-wget https://github.com/strongloop/express/archive/3.18.1.tar.gz
+curl -OL https://github.com/strongloop/express/archive/3.18.1.tar.gz
 tar -zxvf 3.18.1.tar.gz
 rm 3.18.1.tar.gz
 cd express-3.18.1
